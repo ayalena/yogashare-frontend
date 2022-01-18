04c43a813225d0fe3071bce529de73429a739256
ayalena <82506563+ayalena@users.noreply.github.com>
2022-01-18 12:19:51 +0100

    added styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+
+    return (
+        <>
+            <div className="footer-container">
+
+            </div>
+        </>
+    );
+}
+
+export default Footer;
